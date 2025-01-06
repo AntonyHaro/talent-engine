fafced3d9525b2d6557c03f4a344582963795e9d
AntonyHaro <antonyhpro@outlook.com>
2025-01-06 13:20:39 -0300

    melhorias no layout de jobs

--- src/app/jobs/page.js
+++ src/app/jobs/page.js
@@ -1,4 +1,10 @@
 "use client";
+import { IoMdSearch } from "react-icons/io";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { CgWebsite } from "react-icons/cg";
+import { RiRemoteControlLine } from "react-icons/ri";
+import { LuBriefcaseBusiness } from "react-icons/lu";
+import { MdFormatListNumbered } from "react-icons/md";
 
 import { useState } from "react";
 import Link from "next/link";
@@ -10,14 +16,20 @@ export default function Page() {
         location: "",
         siteName: "indeed",
         resultsWanted: 20,
+        isRemote: false,
+        jobType: "fulltime",
     });
     const [jobs, setJobs] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+        const { name, value, type, checked } = e.target;
+        setFormData({
+            ...formData,
+            [name]: type === "checkbox" ? checked : value,
+        });
+        console.log(formData);
     };
 
     const handleSubmit = async (e) => {
@@ -34,6 +46,8 @@ export default function Page() {
                     location: formData.location,
                     site_name: [formData.siteName],
                     results_wanted: formData.resultsWanted,
+                    is_remote: formData.isRemote,
+                    job_type: formData.jobType,
                 }),
             });
 
@@ -55,21 +69,31 @@ export default function Page() {
         <div className={styles.jobs}>
             <h1 className={styles.title}>Busca de Vagas</h1>
             <form onSubmit={handleSubmit} className={styles.form}>
-                <div className={styles.multiColumn}>
-                    <div className={styles.formGroup}>
-                        <label htmlFor="searchTerm">Termo de Busca</label>
-                        <input
-                            type="text"
-                            id="searchTerm"
-                            name="searchTerm"
-                            value={formData.searchTerm}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
+                <div className={styles.formGroup}>
+                    <label htmlFor="searchTerm">
+                        <div className={styles.icon}>
+                            <IoMdSearch />
+                        </div>
+                        Termo de Busca
+                    </label>
+                    <input
+                        type="text"
+                        id="searchTerm"
+                        name="searchTerm"
+                        value={formData.searchTerm}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
 
-                    <div className={styles.formGroup}>
-                        <label htmlFor="location">Localização</label>
+                <div className={styles.formGroup}>
+                    <label htmlFor="location">
+                        <div className={styles.icon}>
+                            <MdOutlineLocationOn />
+                        </div>
+                        Localização
+                    </label>
+                    <div className={styles.multiColumn}>
                         <input
                             type="text"
                             id="location"
@@ -78,12 +102,27 @@ export default function Page() {
                             onChange={handleChange}
                             required
                         />
+                        <label htmlFor="isRemote" className={styles.isRemote}>
+                            Trabalho Remoto?
+                            <input
+                                type="checkbox"
+                                id="isRemote"
+                                name="isRemote"
+                                checked={formData.isRemote}
+                                onChange={handleChange}
+                            />
+                        </label>
                     </div>
                 </div>
 
                 <div className={styles.multiColumn}>
                     <div className={styles.formGroup}>
-                        <label htmlFor="siteName">Site</label>
+                        <label htmlFor="siteName">
+                            <div className={styles.icon}>
+                                <CgWebsite />
+                            </div>
+                            Site
+                        </label>
                         <select
                             id="siteName"
                             name="siteName"
@@ -100,6 +139,9 @@ export default function Page() {
 
                     <div className={styles.formGroup}>
                         <label htmlFor="resultsWanted">
+                            <div className={styles.icon}>
+                                <MdFormatListNumbered />
+                            </div>
                             Quantidade de Resultados
                         </label>
                         <input
@@ -113,13 +155,64 @@ export default function Page() {
                     </div>
                 </div>
 
-                <button
-                    type="submit"
-                    className={styles.button}
-                    disabled={loading}
-                >
-                    {loading ? "Buscando..." : "Buscar Vagas"}
-                </button>
+                <div className={styles.formGroup}>
+                    <label>
+                        <div className={styles.icon}>
+                            <LuBriefcaseBusiness />
+                        </div>
+                        Tipo de Trabalho
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="jobType"
+                            value="fulltime"
+                            checked={formData.jobType === "fulltime"}
+                            onChange={handleChange}
+                        />
+                        Tempo Integral
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="jobType"
+                            value="parttime"
+                            checked={formData.jobType === "parttime"}
+                            onChange={handleChange}
+                        />
+                        Meio Período
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="jobType"
+                            value="internship"
+                            checked={formData.jobType === "internship"}
+                            onChange={handleChange}
+                        />
+                        Estágio
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="jobType"
+                            value="contract"
+                            checked={formData.jobType === "contract"}
+                            onChange={handleChange}
+                        />
+                        Contrato
+                    </label>
+                </div>
+
+                <div className={styles.multiColumn}>
+                    <button
+                        type="submit"
+                        className={styles.button}
+                        disabled={loading}
+                    >
+                        {loading ? "Buscando..." : "Buscar Vagas"}
+                    </button>
+                </div>
             </form>
 
             {error && <p className={styles.error}>Erro: {error}</p>}
